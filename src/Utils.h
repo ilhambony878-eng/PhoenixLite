b587823262967dc932e99babbe04406307919020
ilhambony878-eng <ilhambony878@gmail.com>
2026-07-28 20:45:53 +0700

    Create Utils.h

---
+++ src/Utils.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Utils
+{
+    int Min(int a, int b);
+
+    int Max(int a, int b);
+
+    float Clamp(float value, float min, float max);
+
+    float Lerp(float a, float b, float t);
+}
