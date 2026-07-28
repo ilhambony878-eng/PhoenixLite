1e86ccf402939bfe6150029e812be16f0d66bed1
ilhambony878-eng <ilhambony878@gmail.com>
2026-07-28 17:50:56 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -6,8 +6,9 @@ LOCAL_MODULE := PhoenixLite
 
 LOCAL_SRC_FILES := \
     src/main.cpp \
+    src/Hook.cpp \
     src/Logger.cpp \
-    src/Hook.cpp
+    src/Memory.cpp
 
 LOCAL_CPPFLAGS := -std=c++17
 
