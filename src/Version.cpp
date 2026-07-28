ec184d274d296d7e396dbcef1be0d64139a1b016
ilhambony878-eng <ilhambony878@gmail.com>
2026-07-28 20:21:31 +0700

    Create Version.cpp

---
+++ src/Version.cpp
@@ -0,0 +1,19 @@
+#include "Version.h"
+
+namespace Version
+{
+    const char* Name()
+    {
+        return "PhoenixLite";
+    }
+
+    int Major()
+    {
+        return 1;
+    }
+
+    int Minor()
+    {
+        return 0;
+    }
+}
