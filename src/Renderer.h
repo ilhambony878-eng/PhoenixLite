9fc21ea9065a876ec91311424cf68d7507bfefcf
ilhambony878-eng <ilhambony878@gmail.com>
2026-07-28 19:41:59 +0700

    Update Renderer.h

--- src/Renderer.h
+++ src/Renderer.h
@@ -1,8 +1,17 @@
 #pragma once
 
+#include <GLES3/gl3.h>
+
 namespace Renderer
 {
     bool Init();
+
+    bool LoadShader(
+        const char* vertex,
+        const char* fragment
+    );
+
     void Draw();
+
     void Shutdown();
 }
