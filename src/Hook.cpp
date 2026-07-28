51a111b7399351f7c832f0862a963e56e8f82593
ilhambony878-eng <ilhambony878@gmail.com>
2026-07-28 20:04:56 +0700

    Update Hook.cpp

--- src/Hook.cpp
+++ src/Hook.cpp
@@ -1,7 +1,8 @@
 #include "Hook.h"
 #include "Logger.h"
-#include "Game.h"
+#include "Config.h"
 #include "Memory.h"
+#include "Game.h"
 #include "HookEngine.h"
 #include "Shader.h"
 #include "Renderer.h"
@@ -12,15 +13,21 @@ namespace Hook
     {
         Logger::Info("PhoenixLite Starting...");
 
-        if (!Game::Init())
+        if (!Config::Init())
         {
-            Logger::Error("Game Init Failed");
+            Logger::Error("Config Init Failed");
             return;
         }
 
         if (!Memory::Init())
         {
             Logger::Error("Memory Init Failed");
+            return;
+        }
+
+        if (!Game::Init())
+        {
+            Logger::Error("Game Init Failed");
             return;
         }
 
