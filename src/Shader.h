2a9960305bce8c86558f1c76e6ebdc5d10c58b4c
ilhambony878-eng <ilhambony878@gmail.com>
2026-07-28 18:43:21 +0700

    Create Shader.h

---
+++ src/Shader.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <GLES3/gl3.h>
+
+namespace Shader
+{
+    bool Init();
+
+    GLuint Compile(GLenum type, const char* source);
+
+    GLuint CreateProgram(
+        const char* vertex,
+        const char* fragment
+    );
+
+    void Destroy(GLuint program);
+}
