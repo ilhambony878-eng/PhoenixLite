02babaa26e1712280366a26a2cce28240bcb2d87
ilhambony878-eng <ilhambony878@gmail.com>
2026-07-28 17:44:43 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
 #include <jni.h>
+#include <android/log.h>
+#include <jni.h>
 #include "Hook.h"
+
 
 extern "C"
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*)
 {
+    __android_log_print(
+        ANDROID_LOG_INFO,
+        "PhoenixLite",
+        "PhoenixLite Loaded"
+
+        extern "C"
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*)
+{
     Hook::Init();
+    return JNI_VERSION_1_6;
+}
+
+    );
+
     return JNI_VERSION_1_6;
 }
