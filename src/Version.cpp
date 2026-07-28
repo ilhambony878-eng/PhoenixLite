e0d662cc30a522fceae419a0d6b22eb3ab3f21ef
ilhambony878-eng <ilhambony878@gmail.com>
2026-07-28 20:39:35 +0700

    Update Version.cpp

--- src/Version.cpp
+++ src/Version.cpp
@@ -7,12 +7,22 @@ namespace Version
         return "PhoenixLite";
     }
 
+    const char* Code()
+    {
+        return "v1.0.0";
+    }
+
     int Major()
     {
         return 1;
     }
 
     int Minor()
+    {
+        return 0;
+    }
+
+    int Patch()
     {
         return 0;
     }
