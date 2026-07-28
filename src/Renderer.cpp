438a930bafa6f59396a1b3181bce891dac07e114
ilhambony878-eng <ilhambony878@gmail.com>
2026-07-28 19:06:59 +0700

    Create Renderer.cpp

---
+++ src/Renderer.cpp
@@ -0,0 +1,16 @@
+#include "Renderer.h"
+#include "Logger.h"
+
+namespace Renderer
+{
+    bool Init()
+    {
+        Logger::Info("Renderer Ready");
+        return true;
+    }
+
+    void Render()
+    {
+        // Tempat efek shader nantinya
+    }
+}
