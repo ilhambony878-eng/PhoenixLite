ee8f7d91ecfd83da85e1a90be2922b46d7304304
ilhambony878-eng <ilhambony878@gmail.com>
2026-07-28 18:34:19 +0700

    Update Hook.cpp

--- src/Hook.cpp
+++ src/Hook.cpp
@@ -1,19 +1,28 @@
 #include "Hook.h"
 #include "Game.h"
 #include "Logger.h"
+#include "HookEngine.h"
 
 namespace Hook
 {
     void Init()
     {
-        Logger::Info("Memulai Hook...");
+        Logger::Info("PhoenixLite Starting...");
 
         if (!Game::Init())
         {
-            Logger::Error("Game::Init gagal!");
+            Logger::Error("Game::Init gagal.");
             return;
         }
 
-        Logger::Info("Hook berhasil diinisialisasi.");
+        void* original = nullptr;
+
+        HookEngine::Hook(
+            nullptr,
+            nullptr,
+            &original
+        );
+
+        Logger::Info("PhoenixLite Ready.");
     }
 }
